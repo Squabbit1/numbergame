365447232a16320b5f6f9fdd0e0b7f8591a65f0e
Squabbit1 <nic81addison@gmail.com>
2026-04-22 21:45:33 -0400

    Started styling the buttons

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,10 +1,54 @@
-import {TextInput, View} from 'react-native'
+import { TextInput, View, StyleSheet } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
 
-function StartGameScreen() {}
-    return  (
-        <View>  
-            <TextInput />
+function StartGameScreen() {
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.numberInputContainer}
+        keyboardType="number-pad"
+        maxLength={2}
+      />
+      <View style={styles.buttonContainer}>
+        <PrimaryButton>Reset</PrimaryButton>
+        <PrimaryButton>Confirm</PrimaryButton>
+      </View>
+    </View>
+  );
+}
 
-        </View>
-    );
 export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    alignItems: "center",
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    backgroundColor: "#4e0329",
+    borderRadius: 8,
+    elevation: 4,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "Center",
+    
+  },
+
+  numberInputContainer: {
+    width: 50,
+    fontSize: 32,
+    color: "#ddb52f",
+    fontWeight: "bold",
+    backgroundColor: "#8d2559",
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    marginVertical: 8,
+    textAlign: "center",
+  },
+});
